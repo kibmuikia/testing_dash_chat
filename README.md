54d4656715172707ff4e6001bcecec9c5e93663c
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2023-11-13 21:09:06 +0300

    testing dash_chat_2 v1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test Dash Chat
+
+The most complete Chat UI for flutter
+Easy to use, highly customizable and fully featured
---
+++ android/app/src/main/kotlin/com/example/test_dash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_dash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
